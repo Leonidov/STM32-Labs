81674229692bc33447c599cfc2dd6fe18d1085c7
Vladimir Leonidov <vladimir@leonidov.su>
2021-08-07 14:11:03 +0300

    Добавлен проект "Hello World" на базе CMSIS под CubeIDE.

---
+++ Lab 1 - Hello World/CubeIDE_C/Inc/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h
+ *
+ *  Created on: Aug 7, 2021
+ *      Author: vladimir_leonidov
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f1xx.h"
+
+// Величина задержки между вкл/выкл светодиодов
+#define DELAY_VAL	100000
+
+/* Управление светодиодами */
+#define	LED_ON()	GPIOA->BSRR = GPIO_BSRR_BS5
+#define	LED_OFF()	GPIOA->BSRR = GPIO_BSRR_BR5
+
+/* Прототипы функций */
+void delay(uint32_t takts);
+
+#endif /* MAIN_H_ */
---
+++ Lab 1 - Hello World/CubeIDE_C/Src/main.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * @brief  Основная программа
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;	//включить тактирование GPIOD
+
+	//очистка полей
+	GPIOA->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
+	//и конфигурация
+	GPIOA->CRL |= GPIO_CRL_MODE5_1;		//LD3, выход 2МГц
+
+	//Бесконечный цикл
+	while(1) {
+		LED_ON();						//включить первый светодиод
+		delay(DELAY_VAL);				//вызов подпрограммы задержки
+		LED_OFF();						//выключить первый светодиод
+		delay(DELAY_VAL);				//вызов подпрограммы задержки
+	}
+}
+
+/**
+  * @brief  Подпрограмма задержки
+  * @param  takts - Кол-во тактов
+  * @retval None
+  */
+void delay(uint32_t takts)
+{
+	for (uint32_t i = 0; i < takts; i++) {};
+}
